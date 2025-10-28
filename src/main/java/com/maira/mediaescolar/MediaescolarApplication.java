3b68857b78dfe07cde9688706d68448c1377ebcb
Your Name <you@example.com>
2025-10-28 20:06:58 -0300

    Projeto inicial Spring Boot - media-escolar

---
+++ src/main/java/com/maira/mediaescolar/MediaescolarApplication.java
@@ -0,0 +1,13 @@
+package com.maira.mediaescolar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MediaescolarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MediaescolarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/maira/mediaescolar/MediaescolarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maira.mediaescolar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MediaescolarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
